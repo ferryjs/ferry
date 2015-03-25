853ac8a48251bcbea5a016c33ccacec8938ca0b5
Joshua Jabbour <code@joshuajabbour.com>
2015-03-25 10:01:10 -0700

    Code review clean up.

--- src/Ferry.js
+++ src/Ferry.js
@@ -1,10 +1,10 @@
 'use strict';
 
+import path from 'path';
+
 import Router from './Router';
 import Specification from './Specification';
 import Storage from './Storage';
-
-import path from 'path';
 
 class Ferry {
 
@@ -16,25 +16,25 @@ class Ferry {
 
     if (!(config.specification instanceof Specification)) {
       throw new Error('Specification is required');
-    } else {
-      this.specification = config.specification;
-      this.specification.Ferry = this;
     }
 
     if (!(config.storage instanceof Storage)) {
       throw new Error('Storage is required');
-    } else {
-      this.storage = config.storage;
-      this.storage.Ferry = this;
     }
 
     if (!(config.router instanceof Router)) {
       throw new Error('Router is required');
-    } else {
-      this.router = config.router;
-      this.router.Ferry = this;
-      this.router.initialize(this.specification.basePath, this.specification.routes);
     }
+
+    this.specification = config.specification;
+    this.specification.ferry = this;
+
+    this.storage = config.storage;
+    this.storage.ferry = this;
+
+    this.router = config.router;
+    this.router.ferry = this;
+    this.router.initialize(this.specification.basePath, this.specification.routes);
 
   }
 
